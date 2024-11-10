e97e2d1a29447916498f6b8a1497634f8c8bb7a8
abhin2002 <abhinajith40@gmail.com>
2024-11-10 20:45:12 +0530

    team member add auth token added

--- routes/admin.js
+++ routes/admin.js
@@ -62,7 +62,7 @@ router.get('/get-chatgpt-api-key', authenticateToken, authorizeAdmin, async (req
 
 
 // API to add a new team member by an admin
-router.post('/add-team-member', authorizeAdmin, async (req, res) => {
+router.post('/add-team-member', authenticateToken, authorizeAdmin, async (req, res) => {
     const {
         personalDetails,
         professionalDetails,
