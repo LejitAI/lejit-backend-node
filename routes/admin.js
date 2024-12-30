2d88d931c982649cd4ce5ebb81bfb73be5c5b54f
Mariya Benny <mariyaben02@gmail.com>
2024-12-30 17:45:48 +0530

    updates get-team-member api

--- routes/admin.js
+++ routes/admin.js
@@ -174,15 +174,17 @@ router.delete('/delete-case/:id', async (req, res) => {
 
 
 // API to get all team members
-router.get('/get-team-members', authenticateToken, authorizeAdmin, async (req, res) => {
+router.get('/get-team-members', authenticateToken, async (req, res) => {
     try {
-        const teamMembers = await TeamMember.find({}, '-password'); // Exclude password field for security
+        // Fetch team members created by the logged-in user
+        const teamMembers = await TeamMember.find({ createdBy: req.user.id }, '-password').sort({ createdAt: -1 });
         res.status(200).json(teamMembers);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Failed to retrieve team members. Please try again later.' });
     }
 });
+
 
 
 // Add a new law firm details (including personal, professional, and bank details)
