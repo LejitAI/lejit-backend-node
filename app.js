35477418128706bf2ad5701f53dcd29bc81b1586
Mariyaben <98140958+Mariyaben@users.noreply.github.com>
2025-02-08 06:19:31 +0530

    main

--- app.js
+++ app.js
@@ -11,22 +11,20 @@ const visionRoutes = require('./routes/vision');
 const formatRoutes = require('./routes/format');
 const hearingScheduleRoutes = require('./routes/hearingSchedule');
 const adminModuleRoutes = require('./routes/adminModule');
+const caseRoutes = require('./routes/case');
+const clientRoutes = require('./routes/client');
+const lawFirmRoutes = require('./routes/lawFirm');
+const teamMemberRoutes = require('./routes/teamMember');
 
-
-
-
-
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+const cors = require("cors");
+const corsOptions = {
+   origin: '*', 
+   credentials: true,            // access-control-allow-credentials:true
+   optionSuccessStatus: 200,
 }
 dotenv.config();
 const app = express();
 app.use(cors(corsOptions)) // Use this after the variable declaration
-
-
 
 // Connect to MongoDB
 connectDB();
@@ -38,20 +36,19 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-
-
-// Add the new routes
-app.use('/api/tts', ttsRoute);
-app.use('/api/speech-to-text', speechToTextRoute);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/tts', ttsRoute);
+app.use('/api/speech-to-text', speechToTextRoute);
 app.use('/api/vision', visionRoutes);
 app.use('/api/format', formatRoutes);
 app.use('/api/hearing-schedule', hearingScheduleRoutes);
 app.use('/api/adminModule', adminModuleRoutes);
-
-
+app.use('/api/case', caseRoutes);
+app.use('/api/client', clientRoutes);
+app.use('/api/law-firm', lawFirmRoutes);
+app.use('/api/team-member', teamMemberRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
